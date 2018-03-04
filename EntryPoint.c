6e562039e726f5d707d48534bdfbec3093ba5174
root <root@OSC>
2018-03-04 12:58:40 -0700

    Added history, background process support, pipelining option(needs more work)

--- EntryPoint.c
+++ EntryPoint.c
@@ -5,9 +5,11 @@
 #include <sys/wait.h>
 
 #define CMDMAX 80
+#define HISTMAX 100
 #define TOKENSMAX 50
 #define TOKENLEN 1000
 #define MAX_LEN 128
+#define ALLOWPIPELINING 0
 
 void tokenize(char* input, char* argv[])
 {
@@ -17,6 +19,9 @@ void tokenize(char* input, char* argv[])
 		of tokens is 50, and max token length is
 		1000
 	*/
+	char* tempStorage = (char*)malloc(strlen(input)*sizeof(char));
+	strcpy(tempStorage, input);
+
 	char* tokens = strtok(input, " \n");
 	int tokenIndex = 0;
 
@@ -28,6 +33,7 @@ void tokenize(char* input, char* argv[])
 	}
 
 	argv[tokenIndex] = NULL;
+	strcpy(input, tempStorage);
 }
 
 void printWelcomeMessage()
@@ -64,13 +70,14 @@ void getCommand(char** input)
 		(contrived I know)
 	*/
 	size_t inputSize = CMDMAX;
-
-	if(getline(&*input, &inputSize, stdin) < 0)
-	{
-		printf("%s\n", "Unable to allocate input\
-			string");
-		exit(-1);
-	}
+	do{
+		if(getline(&*input, &inputSize, stdin) < 0)
+		{
+			printf("%s\n", "Unable to allocate input\
+				string");
+			exit(-1);
+		}
+	}while(!strcmp(*input, "\n"));
 }
 
 void printStringArray(char* strArray[])
@@ -82,57 +89,185 @@ void printStringArray(char* strArray[])
 	}
 }
 
+int notExit(char* input){
+	return strcmp(input,"exit");
+}
+
+int isExit(char* input){
+	return !strcmp(input,"exit");
+}
+
+void saveCommandToHistory(char** history, char* cmd, int index){
+	history[index] = (char*)malloc(CMDMAX*sizeof(char));
+	strcpy(history[index], cmd);
+}
+
+int isHistoryRequest(char* cmd){
+	return !(strcmp(cmd, "!!") && strcmp(cmd, "!"));
+}
+
+void clearStringArray(char* arr[], int arrMax)
+{
+	int i = 0;
+	for(i = 0; i<arrMax; i++)
+	{
+		arr[i] = "\0";
+	}
+}
+
+int isBackgroundProcess(char* argv[])
+{
+	int i = 0;
+	while(argv[i] != NULL)
+	{
+		if(!strcmp(argv[i++],"&"))
+		{
+			free(argv[i-1]);
+			argv[i-1] = NULL; //remove symbol
+			return 1;
+		}
+	}
+	return 0;
+}
+
 int main(){
 
 
 	printWelcomeMessage();
 
+	int backgroundFlag = 0;
+
+	int historyIndex = 0;
+	char* history[HISTMAX];
+	clearStringArray(history, HISTMAX);
+
 	char* input = NULL;
 	getCommand(&input);
 
 	char* argv[TOKENSMAX];
+	clearStringArray(argv, TOKENSMAX);
 	tokenize(input, argv);
 
-	pid_t pid;
-	int fd[2];
-
-	int p = pipe(fd);
-	if(p<0)
+	//doesn't make sense to make a history request here
+	//so prevent it
+	while(isHistoryRequest(argv[0]))
 	{
-		printf("%s\n", "Failed to pipe");
-		exit(1);
+		getCommand(&input);
+		tokenize(input, argv);
 	}
 
-	pid = fork();
-	printf("%u",pid);
-	if (pid < 0)
+	if(isBackgroundProcess(argv))
 	{
-		printf("%s\n", "Fork Failed");
-		exit(1);
-	}
-	else if (pid == 0)
-	{	
-		//remove stdout from fdlist
-		close(1);
-		//replace it with pipe write
-		dup(fd[1]);
-		//close pipe
-		close(fd[0]);
-		close(fd[1]);
-		//initiate exec
-		execvp(argv[0],argv);
-	}
-	else
-	{
-		char buffer[4096];
-		close(fd[1]);
-		int bytes = read(fd[0],buffer,sizeof(buffer));
-		printf("%u\n", bytes);
-		printf("%s\n", buffer);
-		wait(NULL);
+		backgroundFlag = 1;
 	}
 
+	if(isExit(argv[0]))
+	{
+		return 0;
+	}
+
+	saveCommandToHistory(history, input, historyIndex++);
+	
+	do{
+
+		//process command
+
+		pid_t pid;
+		int fd[2];
+
+		int p = pipe(fd);
+		if(p<0)
+		{
+			printf("%s\n", "Failed to pipe");
+			exit(1);
+		}
+
+		pid = fork();
+		printf("%u",pid);
+		if (pid < 0)
+		{
+			printf("%s\n", "Fork Failed");
+			exit(1);
+		}
+		else if (pid == 0)
+		{	
+			if(ALLOWPIPELINING)
+			{
+				close(1);
+				//replace it with pipe write
+				dup(fd[1]);
+				//close pipe
+				close(fd[0]);
+				close(fd[1]);
+				//initiate exec
+			}
+			//remove stdout from fdlist
+			execvp(argv[0],argv);
+		}
+		else
+		{
+			if(ALLOWPIPELINING)
+			{
+				char buffer[4096] = "";
+				close(fd[1]);
+				read(fd[0],buffer,sizeof(buffer));
+				printf("%s\n", buffer);
+			}
+			else{
+				if(backgroundFlag == 0)
+				{
+					wait(NULL);
+				}
+				else
+				{
+					backgroundFlag = 0; //reset background flag if it was a background process
+				}
+			}
+		}
+
+		//get new command
+		int invalidHistoryIndex = 0;
+
+		getCommand(&input);
+		tokenize(input, argv);
+
+		do{
+
+			if(isHistoryRequest(argv[0]))
+			{
+				if(argv[1] == NULL) // just !!
+				{
+					invalidHistoryIndex = 0;
+					printf("%s\n", history[historyIndex-1]);
+					tokenize(history[historyIndex-1], argv);
+				}
+				else if(atoi(argv[1]) < historyIndex) // !! with index
+				{
+					invalidHistoryIndex = 0;
+					printf("%s\n", history[atoi(argv[1])]);
+					tokenize(history[atoi(argv[1])], argv);
+				}
+				else
+				{
+					invalidHistoryIndex = 1;
+					printf("%s\n", "Invalid history index");
+					getCommand(&input);
+					tokenize(input, argv);
+				}
+			}
+			else
+			{
+				saveCommandToHistory(history, input, historyIndex++);
+			}
+
+		}while(invalidHistoryIndex);
+
+		if(isBackgroundProcess(argv))
+		{
+			backgroundFlag = 1;
+		}
+
+	}while(notExit(argv[0]));	
 
 	return 0;
 }
-
